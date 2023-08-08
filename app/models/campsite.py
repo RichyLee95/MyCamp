f71d9797a2e304af0c96dfbb17bb57351acf94bf
Richard <richylee95@gmail.com>
2023-08-08 14:14:58 -0700

    added campsite model

---
+++ app/models/campsite.py
@@ -0,0 +1,32 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Campsite(db.Model):
+    __tablename__ = "campsites"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    hours = db.Integer(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+#relationship attributes    
+    users = db.relationship("User", back_populates="campsites")
+    attractions= db.relationship("Attraction", back_populates="campsites")
+    reviews = db.relationship("Review", back_populates="campsites")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user": self.users.to_dict(),
+            "title": self.title,
+            "address": self.address,
+            "hours": self.hours,
+            "image": self.image,
+            "reviews":[review.to_dict() for review in self.reviews],
+            "attractions":[attraction.to_dict() for attraction in self.attractions],
+            "reviews_count": len(self.reviews),
+        }
+    
