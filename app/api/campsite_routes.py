b6a939fe7d34bb1a6071c705a5e687e0b2df1529
Richard <richylee95@gmail.com>
2023-08-10 14:47:29 -0700

    finally got post campsites working

--- app/api/campsite_routes.py
+++ app/api/campsite_routes.py
@@ -39,18 +39,31 @@ def add_campsite():
   form = CampsiteForm()
   form['csrf_token'].data = request.cookies['csrf_token']
 
-
   if form.validate_on_submit():
+    image = form.data["image"]
+    image.filename = get_unique_filename(image.filename)
+    upload = upload_file_to_s3(image)
+
+    prev_image = form.data["prev_image"]
+    prev_image.filename = get_unique_filename(prev_image.filename)
+    upload_prev = upload_file_to_s3(prev_image)
+
+    if "url" not in upload:
+      return {"error": "upload failed!"}
+  
+
+  
     new_campsite = Campsite(
+      user_id = current_user.id,
       title = form.data["title"],
-      # address =
-      # hours_open =
-      # hours_close =
-      # phone_number =
-      # image =
-      # prev_image =
-      users = current_user,
+      address =form.data["address"],
+      hours_open =form.data["hours_open"],
+      hours_close =form.data["hours_close"],
+      phone_number =form.data["phone_number"],
+      image =upload["url"],
+      prev_image =upload_prev["url"]
     )
+    
 
     db.session.add(new_campsite)
     db.session.commit()
@@ -73,8 +86,24 @@ def update_campsite(id):
 
     if not campsite_to_update:
       return {"errors": f"campsite with id {id} does not exist"}
+    
+    if form.data['image']:
+      image=form.data["image"]
+      image.filename=get_unique_filename(image.filename)
+      upload = upload_file_to_s3(image)
+      campsite_to_update.image=upload["url"]
+
+    if form.data['prev_image']:
+      prev_image=form.data["prev_image"]
+      prev_image.filename=get_unique_filename(prev_image.filename)
+      upload_prev = upload_file_to_s3(prev_image)
+      campsite_to_update.prev_image=upload_prev["url"]
+
     campsite_to_update.user = current_user
-    campsite_to_update.content = form.data["content"]
+    campsite_to_update.title = form.data["title"]
+    campsite_to_update.hours_open = form.data["hours_open"]
+    campsite_to_update.hours_close = form.data["hours_close"]
+    campsite_to_update.phone_number = form.data["phone_number"]
 
     db.session.commit()
     return {"updatedCampsite": campsite_to_update.to_dict()}
--- app/forms/campsite_form.py
+++ app/forms/campsite_form.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateTimeField, SubmitField
+from wtforms import StringField, TimeField, SubmitField
 from wtforms.validators import DataRequired, Length
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from ..api.AWS_helpers import ALLOWED_EXTENSIONS
 class CampsiteForm(FlaskForm):
   title = StringField("Title", validators=[DataRequired(), Length(max=255, min=5)])
   address = StringField("Address", validators=[DataRequired(), Length(max=255)])
-  hours_open = DateTimeField("Hours Open", validators=[DataRequired()],format="%H:%M")
-  hours_close = DateTimeField("Hours Closed",validators=[DataRequired()],format="%H:%M")
+  hours_open = TimeField("Hours Open", validators=[DataRequired()],format="%H:%M")
+  hours_close = TimeField("Hours Closed",validators=[DataRequired()],format="%H:%M")
   phone_number = StringField("Phone Number", validators=[DataRequired()])
   image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
-  prev_image = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+  prev_image = FileField("Prev_Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
   submit = SubmitField("Submit")
